7a76569f1352458e9f9a37e16adbd50a0adfd476
Pierre <pierre.a.lafortune@gmail.com>
2014-07-26 09:12:51 -0400

    My additions in green
    I briefly described my code at various points in the structure.

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,31 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## This code, by Pierre Lafortune, takes a user defined matrix, and creates four functions to manipulate and save (cache) the data.
+makeCacheMatrix <- function(x = matrix()) { ##x is a user defined matrix
+        m <- NULL
+        set <- function(y) { ## user can change the matrix to be solved. Note the <<- assignment operator. It will allow x and y to be applied beyond the 'set' environment.
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get, ## creates a list of the four functions
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+## cacheSolve is a function that first checks for a NULL m set. If it is empty, it will find the inverse of the matrix and save the value.
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data") ## if it isn't empty, it will retrieve the cached(saved) value.
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)## this code solves the matrix
+        x$setinverse(m) ## user can use this to test another matrix
+        m
+}
